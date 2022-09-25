6846b41b7c60fb4b318ff34dfb1a99dce6ae74d3
Anmol Jhamb <talktoanmol@outlook.com>
2022-09-25 21:53:43 +0530

    RECURSION - BIN_STRINGS WITHOUT CONSECUTIVE ONES

---
+++ RECURSION/bin_strings_alter.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector> 
+#include <string>
+using namespace std;
+
+// there shouldn't be any consecutive ones.
+
+void bin_string(int n, vector<string> &arr, int i=0)
+{
+    if ( n == 1 )
+    {
+        arr.push_back("0");
+        arr.push_back("1");
+        return;
+    }
+
+    bin_string(n-1, arr);
+    vector<string> temp_arr; 
+    
+    for ( auto x : arr )
+    {
+        temp_arr.push_back("0"+x);
+    }
+
+    for ( auto x : arr )
+    {
+        if ( x[0] != '1' )
+            temp_arr.push_back("1"+x);
+    }
+    arr = temp_arr;
+}
+
+
+int main()
+{
+    vector<string> arr;
+    int n = 3; 
+    bin_string(n, arr);
+
+    for ( auto x : arr )
+    {
+        cout << x << endl;
+    }
+
+    return 0;
+}
